31e2f9092061fe148ad79cba8f1a4328b1748190
Jesse Palmer <pravinda333@gmail.com>
2019-08-12 03:46:12 +0530

    smooth scrolling and onviewenter added

--- js/js.js
+++ js/js.js
@@ -88,3 +88,69 @@ document.body.addEventListener('click', function(e){
 
 const landingh1 = document.querySelector('.landing h1');
 
+let header = document.querySelector('.header');
+
+let invis = document.querySelector('.invisible');
+let last_scrollY = window.scrollY;
+let scrolling = false;
+window.addEventListener('scroll', (e) => {
+	if (last_scrollY == window.scrollY){
+		return;
+	}
+
+	if (last_scrollY > window.scrollY){
+		//scroll up`
+		if (!scrolling){
+			window.scroll({top:0, left:0, behavior:'smooth'});
+			scrolling = true;
+		}
+	}else{
+		//scroll down
+		if (!scrolling && window.scrollY < header.clientHeight){
+			window.scroll({top: header.clientHeight, left:0, behavior:'smooth'});
+			scrolling = true;
+		}
+
+	}
+	if (window.scrollY == header.clientHeight || window.scrollY == 0){
+		scrolling = false;
+	}
+	last_scrollY = window.scrollY;
+
+	if (isInViewport(invis)){
+		invis.style.visibility = 'visible';
+		invis.style.left = '0';
+		invis.style.opacity = 1;
+	}else{
+		invis.style.visibility = 'hidden';
+		invis.style.opacity = 0;
+		invis.style.left = '500px';
+	}
+});
+function isInViewport(el) {
+
+    const rect = el.getBoundingClientRect();
+    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    return (vertInView && horInView);
+}
+function isInView(element){
+	// Get it's position in the viewport
+	var bounding = element.getBoundingClientRect();
+	if (
+		bounding.top >= 0 &&
+		bounding.left >= 0 &&
+		bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+		bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+	) {
+		return true;
+	} else {
+		return false;
+	}
+}
